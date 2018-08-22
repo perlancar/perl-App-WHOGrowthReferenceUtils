644ae8308f6bed6920b86515665944402facd6fd
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-08-22 12:15:13 +0700

    Initial commit

---
+++ lib/App/WHOGrowthReferenceUtils.pm
@@ -0,0 +1,20 @@
+package App::WHOGrowthReferenceUtils;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: Utilities related to WHO growth reference
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<WHO::GrowthReference::Table>
+
+=cut
